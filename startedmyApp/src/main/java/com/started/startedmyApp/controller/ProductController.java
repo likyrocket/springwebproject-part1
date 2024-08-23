9b73986eaed0028385aab03110aae414f9ea3f76
likyrocket <likyrocket@gmail.com>
2024-08-23 01:13:30 -0400

    springweb Crud operations without db

--- startedmyApp/src/main/java/com/started/startedmyApp/controller/ProductController.java
+++ startedmyApp/src/main/java/com/started/startedmyApp/controller/ProductController.java
@@ -4,7 +4,12 @@ package com.started.startedmyApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.started.startedmyApp.model.Product;
@@ -17,9 +22,26 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProductById(prodId);
+    }
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        service.addProduct(prod); 
+
+    }
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
     }
     
 }
--- startedmyApp/src/main/java/com/started/startedmyApp/service/ProductService.java
+++ startedmyApp/src/main/java/com/started/startedmyApp/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.started.startedmyApp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,15 +12,44 @@ import com.started.startedmyApp.model.Product;
 public class ProductService {
     
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
         new Product(101,"Iphone",50000),
         new Product(102,"Canon",70000),
-        new Product(103,"shure mic",10000));
+        new Product(103,"shure mic",10000)));
 
     public List<Product> getProducts(){
 
         return products;
         
     }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+            .filter(p-> p.getProdId() == prodId)
+            .findFirst().get();
+    }
+    public void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index=0;
+        for(int i=0;i<products.size();i++)
+           if(products.get(i).getProdId() == prod.getProdId())
+               index=i;
+        
+        products.set(index,prod);   
+    }
+
+    public void deleteProduct(int prodId) {
+        int index=0;
+        for(int i=0;i<products.size();i++)
+           if(products.get(i).getProdId() == prodId)
+               index=i;
+        products.remove(index);
+        
+    }
+
+    
     
 }
